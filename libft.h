a85b5d468f871ef2ada9ba7c241bdf9ef89695ca
Jenna Mustajärvi <jemustaj@student.42porto.com>
2024-10-30 17:06:57 +0000

    added new: ft_isalnum.c ft_isalpha.c ft_isascii.c ft_isdigit.c ft_isprint.c ft_strlen.c libft.h

---
+++ libft.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jemustaj <jemustaj@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/29 16:17:45 by jemustaj          #+#    #+#             */
+/*   Updated: 2024/10/29 22:16:36 by jemustaj         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+#define LIBFT_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <ctype.h> //isalpha, isdigit, isalnum, isascii, isprint
+#include <string.h> // strlen
+
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+int	ft_isalnum(int c);
+int	ft_isascii(int c);
+int	ft_isprint(int c);
+size_t	ft_strlen(const char *s);
+
+#endif
